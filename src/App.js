1dbf0e4cd70e1040026895f9a161e1a5b986bfbb
unknown <FrontEnd@PCAU0941.formart.it>
2018-12-05 13:45:54 +0100

    Fetch

--- src/App.js
+++ src/App.js
@@ -1,28 +1,64 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Nav, NavItem, Dropdown, DropdownItem, DropdownToggle, DropdownMenu, NavLink } from 'reactstrap';
+import monogramma from "./monogramma.jpg"
+import Post from "./components/post.js"
 
-class App extends Component {
+
+class Example extends React.Component {constructor(props) {
+  super(props);
+
+  this.toggle = this.toggle.bind(this);
+  this.state = {
+    dropdownOpen: false
+  };
+}
+
+toggle() {
+  this.setState({
+    dropdownOpen: !this.state.dropdownOpen
+  });
+}
+
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <img src={monogramma}  className="image"/>
+         <div className="menu">
+        <Nav pills>
+          <NavItem>
+            <NavLink href="#" active>Home</NavLink>
+          </NavItem>
+          <Dropdown nav isOpen={this.state.dropdownOpen} toggle={this.toggle}>
+            <DropdownToggle nav caret>
+              Servizi
+            </DropdownToggle>
+            <DropdownMenu>
+              <DropdownItem >Servizio 1</DropdownItem>
+              <DropdownItem >Servizio 2</DropdownItem>
+              <DropdownItem>Servizio 3</DropdownItem>
+              <DropdownItem divider />
+              <DropdownItem>Another Action</DropdownItem>
+            </DropdownMenu>
+          </Dropdown>
+          <NavItem>
+            <NavLink href="#">Contatti</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#">Dove Siamo</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink disabled href="#">Lavora con noi</NavLink>
+          </NavItem>
+        </Nav>
+      </div>
+
+      <Post></Post>
+    
       </div>
     );
   }
 }
 
-export default App;
+export default Example;
---
+++ src/components/post.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+
+
+class Post extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            post: []
+        }
+    }
+
+    PostFetch() {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => this.setState({
+                post: json
+
+            }))
+    }
+
+
+    render() {
+        return (
+
+            <div className="post">
+                {this.state.post.map((item) => 
+                    <div>
+                    <div>{item.id}</div>
+                        <div>{item.userId}</div>
+                        <div>{item.title}</div>
+                        <div>{item.body}</div>
+               </div>
+            )
+                }
+            </div>
+
+        )
+
+
+    }
+}
+export default Post
+
+
